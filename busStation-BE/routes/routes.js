c8a8e44bc975140007816ba8d6b7b497337cdfdc
Kiril Kyuchukov <kiril_kuchukov@abv.bg>
2018-09-28 23:20:53 +0300

    Add transport company names

--- busStation-BE/routes/routes.js
+++ busStation-BE/routes/routes.js
@@ -19,12 +19,12 @@ var appRouter = function (app) {
 
     app.get("/schedule/:num", function (req, res) {
         var schedules = [];
+        var transportCompany = ["myTransport", "Transport PROS", "TravelTO", "Instant Arrival"];
         var num = req.params.num;
-
         if (isFinite(num) && num > 0) {
             for (i = 0; i <= num - 1; i++) {
                 schedules.push({
-                    transportCompany: faker.name.title(),
+                    transportCompany: transportCompany[Math.floor(Math.random() * Math.floor(4))],
                     startingPoint: faker.address.city(),
                     destination: faker.address.city(),
                     departureTime: faker.date.recent(),
